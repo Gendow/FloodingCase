3bf6b695ccd1b2e4bd554d37fa4af9ce887156a0
Rony <rbnedkov@gmail.com>
2015-04-22 11:13:13 +0200

    updated processing script for experiments 2 and 3

--- Processing_output.R
+++ Processing_output.R
@@ -6,14 +6,18 @@ library(rgeos)
 studarea <- readOGR("data/study_area.shp", "study_area")
 
 
-inroot <- "D:/Geostat/FloodingCase/export/output/experiment1/" # change this
+inroot1 <- "D:/Geostat/FloodingCase/export/output/experiment1/" # change this
+inroot2 <- "D:/Geostat/FloodingCase/export/output/experiment2/" # change this
+inroot3 <- "D:/Geostat/FloodingCase/export/output/experiment3/" # change this
+
 mask <- readGDAL("data/mask100.asc")
+
 # Read all flooding model results into a matrix "fr"
-flist <- list.files(inroot) # all file names in directory
+flist <- list.files(inroot1) # all file names in directory
 fr = numeric()
 for(in_name in flist)
 {
-  in_name <- paste(inroot, in_name, sep = "")
+  in_name <- paste(inroot1, in_name, sep = "")
   fr <- cbind(fr,(readGDAL(in_name, silent = TRUE))$band1)
 }
 fr <- ifelse(is.na(fr), 0, fr) # set NA to 0
@@ -36,13 +40,94 @@ prob$sd <- apply(fr, 1 , function(x) sd(x))
 prob$mean <- apply(fr, 1 , function(x) mean(x))
 
 
-
 spplot(prob, zcol = "mean", main = "Mean of flooding", col.regions = colorRampPalette(c("white", "blue"))(30), 
        sp.layout = list("sp.polygons", studarea, first = F))
        
-spplot(prob, zcol = "Variance", main = "Variance of flooding", col.regions = colorRampPalette(c("white", "green", "red", "yellow"))(30), 
+spplot(prob, zcol = "Variance", main = "Variance of flooding", col.regions = colorRampPalette(c("white", "green", "yellow", "red"))(30), 
        sp.layout = list("sp.polygons", studarea, first = F))
-spplot(prob, zcol = "sd", main = "Sdv of flooding", col.regions = colorRampPalette(c("white", "green", "red", "yellow"))(30), 
+spplot(prob, zcol = "sd", main = "Sdv of flooding", col.regions = colorRampPalette(c("white", "green", "yellow", "red"))(30), 
+       sp.layout = list("sp.polygons", studarea, first = F))
+
+
+
+
+#Experiment 2
+
+
+# Read all flooding model results from second experiment into a matrix "exp2"
+flist2 <- list.files(inroot2) # all file names in directory
+exp2 = numeric()
+for(in_name in flist2)
+{
+  in_name <- paste(inroot2, in_name, sep = "")
+  exp2 <- cbind(exp2,(readGDAL(in_name, silent = TRUE))$band1)
+}
+exp2 <- ifelse(is.na(exp2), 0, exp2) # set NA to 0
+
+# Compute probability of flooding
+prob2 <- apply(exp2, 1, function(x) mean(ifelse(x > 0, 1, 0)))
+prob2 <- prob2 * mask$band1 # probs; get rid of NA area
+
+# Convert prob to SpatialPixelsDataFrame
+prob2 <- as.data.frame(prob2)
+coordinates(prob2) <- coordinates(mask)
+gridded(prob2) <- TRUE
+
+# plot using blue, pink, yellow colorramp
+spplot(prob2, zcol=1, col.regions=bpy.colors())
+
+#Calculation and plotting of mean and variance per grid cell
+prob2$variance <- apply(exp2, 1 , function(x) var(x))
+prob2$sd <- apply(exp2, 1 , function(x) sd(x))
+prob2$mean <- apply(exp2, 1 , function(x) mean(x))
+
+
+spplot(prob2, zcol = "mean", main = "Mean of flooding", col.regions = colorRampPalette(c("white", "blue"))(30), 
+       sp.layout = list("sp.polygons", studarea, first = F))
+
+spplot(prob2, zcol = "variance", main = "Variance of flooding", col.regions = colorRampPalette(c("white", "green", "yellow", "red"))(30), 
+       sp.layout = list("sp.polygons", studarea, first = F))
+spplot(prob2, zcol = "sd", main = "Sdv of flooding", col.regions = colorRampPalette(c("white", "green", "yellow", "red"))(30), 
+       sp.layout = list("sp.polygons", studarea, first = F))
+
+
+
+#experiment 3
+
+# Read all flooding model results from second experiment into a matrix "exp2"
+flist3 <- list.files(inroot3) # all file names in directory
+exp3 = numeric()
+for(in_name in flist3)
+{
+  in_name <- paste(inroot3, in_name, sep = "")
+  exp3 <- cbind(exp3,(readGDAL(in_name, silent = TRUE))$band1)
+}
+exp3 <- ifelse(is.na(exp3), 0, exp3) # set NA to 0
+
+# Compute probability of flooding
+prob3 <- apply(exp3, 1, function(x) mean(ifelse(x > 0, 1, 0)))
+prob3 <- prob3 * mask$band1 # probs; get rid of NA area
+
+# Convert prob to SpatialPixelsDataFrame
+prob3 <- as.data.frame(prob3)
+coordinates(prob3) <- coordinates(mask)
+gridded(prob3) <- TRUE
+
+# plot using blue, pink, yellow colorramp
+spplot(prob3, zcol=1, col.regions=bpy.colors())
+
+#Calculation and plotting of mean and variance per grid cell
+prob3$variance <- apply(exp3, 1 , function(x) var(x))
+prob3$sd <- apply(exp3, 1 , function(x) sd(x))
+prob3$mean <- apply(exp3, 1 , function(x) mean(x))
+
+
+spplot(prob3, zcol = "mean", main = "Mean of flooding", col.regions = colorRampPalette(c("white", "blue"))(30), 
+       sp.layout = list("sp.polygons", studarea, first = F))
+
+spplot(prob3, zcol = "variance", main = "Variance of flooding", col.regions = colorRampPalette(c("white", "green", "yellow", "red"))(30), 
+       sp.layout = list("sp.polygons", studarea, first = F))
+spplot(prob3, zcol = "sd", main = "Sdv of flooding", col.regions = colorRampPalette(c("white", "green", "yellow", "red"))(30), 
        sp.layout = list("sp.polygons", studarea, first = F))
 
 
